ce019c5d8a647e9af45ec178c4c7fd25616664a2
engrmuqeetahmad <engrmuqeetahmad@gmail.com>
2026-07-01 16:51:19 +0000

    added config maps, secrets, and updated nestjs code to read env variables with config isGLobal true

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheckService, HttpHealthIndicator, HealthCheck } from '@nestjs/terminus';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
-  ) { }
+private config: ConfigService
+  ) {
+ console.log(this.config.get('PORT'));
+ console.log(this.config.get('ENVIRONMENT'));
+ }
 
   @Get()
   @HealthCheck()
